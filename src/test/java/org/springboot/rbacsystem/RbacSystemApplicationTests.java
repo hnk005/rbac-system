250778f26fadcd0cc16747f8a4e2d041aff75a4c
HNK <huynhnamkha512020@gmail.com>
2026-03-15 04:51:48 +0700

    initial project

---
+++ src/test/java/org/springboot/rbacsystem/RbacSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.rbacsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacSystemApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
